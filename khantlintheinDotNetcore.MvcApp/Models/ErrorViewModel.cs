a8a1a6e52f66ad3245362f02272d844b5bccf76d
khant lin thein <minkhant1572003@gmail.com>
2024-02-24 23:19:15 +0630

    Processing MVC App

---
+++ khantlintheinDotNetcore.MvcApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace khantlintheinDotNetcore.MvcApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
